137ce998b784509828db846d1fb3e4f8267aa936
JinTianBuXiangShangBan <shadow6217@gmail.com>
2024-05-23 17:33:54 +0800

    chore: update pwa icon

--- build/vite/index.ts
+++ build/vite/index.ts
@@ -96,17 +96,17 @@ export function createVitePlugins() {
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/pwa192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/pwa512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/pwa512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
